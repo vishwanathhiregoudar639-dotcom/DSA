8e229b6bf1454a2b442fdd46ba2c6ca7261ead14
vishwanathhiregoudar639-dotcom <vishwanathhiregoudar639@gmail.com>
2026-05-30 10:47:20 +0530

    Update ll.cpp

--- ll.cpp
+++ ll.cpp
@@ -47,5 +47,41 @@ int main() {
     }
 
     return 0;
+}#include<iostream>
+#include<list>
+#include<iterator>
+using namespace std;
+
+void printList(list<int>ll){
+    list<int>::iterator iter;
+    for(iter = ll.begin();iter != ll.end();iter++){
+        cout << (*iter) << "->";
+    }
+    cout << "NULL" << endl;
 }
+
+int main(){
+    list <int>ll;
+
+    ll.push_front(2);
+    ll.push_front(1);
+
+    ll.push_back(3);
+    ll.push_back(4);
+    
+    printList(ll);
+    cout << ll.size() << endl;
+
+    cout << "head=" << ll.front()<<"\n";
+    cout << "tail=" << ll.back()<<"\n";
+
+    ll.pop_front();
+    printList(ll);
+    
+    ll.pop_back();
+    printList(ll);
+
+    return 0;
+}
+
 
